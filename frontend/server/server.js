cff36d4c33feea401c00e276017460166ebbe67d
Tibor Vito Susnjara <tibor.susnjara@student.um.si>
2025-10-07 12:29:06 +0200

    fixed incorrect path parameter

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const path = require('path');
 
 dotenv.config();
 
@@ -8,8 +9,9 @@ const port = process.env.PORT || 4000
 
 app.use(express.static("dist"));
 
+const indexHtml = path.resolve(__dirname, "./dist/index.html");
 app.use("*", (req, res) => {
-    res.sendFile("dist/index.html")
+    res.sendFile(indexHtml)
 })
 
 
