b3d7f972b4109c06e431f77337c3d0d3ef9730e3
Tibor Vito Susnjara <tibor.susnjara@student.um.si>
2025-12-06 17:01:40 +0100

    added test fixture for login context + some test tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,44 @@
 import re
-#import os
+import os
 from playwright.sync_api import Page, expect
+import pytest
+from dotenv import load_dotenv
 
-PAGE_ROOT = "http://localhost:4000"
+load_dotenv()
+
+SITE_ROOT = os.environ["SITE_ROOT"]
+TEST_USER_EMAIL = os.environ["TEST_USER_EMAIL"]
+TEST_USER_PASSWORD = os.environ["TEST_USER_PASSWORD"]
+
+LOGIN_STATE_LOCATION = ".auth/state.json"
+
+@pytest.fixture(scope="session")
+def login(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(SITE_ROOT + "/login")
+    page.get_by_role("textbox", name="E-mail").click()
+    page.get_by_role("textbox", name="E-mail").fill(TEST_USER_EMAIL)
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill(TEST_USER_PASSWORD)
+    page.get_by_role("button", name="Log in").click()
+    context.storage_state(path=LOGIN_STATE_LOCATION)
+    context.close()
+
+    return LOGIN_STATE_LOCATION
+
+@pytest.fixture
+def user_logged_in(browser, login):
+    context = browser.new_context(storage_state=login)
+    yield context
+    context.close()
 
 def test_has_title(page: Page):
-    page.goto(PAGE_ROOT)
+    page.goto(SITE_ROOT)
 
     expect(page).to_have_title(re.compile("Yet Another Forum"))
+
+def test_is_username_displayed(user_logged_in):
+    page = user_logged_in.new_page()
+    page.goto(SITE_ROOT)
+    expect(page.get_by_text("abc").first).to_be_visible()
